b5fe5876e4cb991dda4817064f0202e0ad7ff3ec
krechak <krecha_1998@tlen.pl>
2021-04-23 13:19:52 +0000

    dziala dla kazdego pina z wykorzystaniem wiringpi

--- led_1.sh
+++ led_1.sh
@@ -4,10 +4,7 @@ PINS_INPUT="20"
 PINS_OUTPUT="21"
 PINS="20 21"
 KEY_1="20"
-#KEY_2="3"
 LED_1="21"
-#LED_2="2"
-
 
 set_pin_mode()
 {
@@ -47,46 +44,33 @@ key_led_action()
 {
 	if [ $(cat /sys/class/gpio/gpio$1/value) -eq "1" ]; then
 		echo "0" > /sys/class/gpio/gpio$2/value	
-		echo "stan NISKI na pinie $2 spowodowany wciśnięciem przycisku $1"
+		echo "stan WYSOKI na pinie $2 spowodowany wciśnięciem przycisku $1"
 	else
 		echo "1" > /sys/class/gpio/gpio$2/value	
-		echo "stan WYSOKI na pinie $2 spowodowany wciśnięciem przycisku $1"
+		echo "stan NISKI na pinie $2 spowodowany wciśnięciem przycisku $1"
 	fi
 }
 
+key_pull_up()
+{
+	for i in $1
+	{
+		gpio -g mode $1 up
+	}
+}
 
-# check_inputs()
-# {
-# 	for i in $1
-# 	do
-# 		if [ $(cat /sys/class/gpio/gpio$i/value) -eq "1" ]; then
-			
-# 			echo "0" > /sys/class/gpio/gpio$i/value	
-# 		else
-			
-# 		fi
-# 	done
-# }
+led_pull_off()
+{
+	for i in $1
+	{
+		gpio -g mode $1 tri
+	}
+}
 
-
-
-# check_inputs "$PINS_INPUT" "${LEDS[@]}"
-
-# while [ 1 ]
-# do
-# 	x=$(cat /sys/class/gpio/gpio3/value)
-# 	if [ "$x" = "1" ]
-# 	then
-# 		echo "0" > /sys/class/gpio/gpio2/value	
-# 	else
-# 		echo "1" > /sys/class/gpio/gpio2/value
-# 	fi	
-# 	#sleep 0.1
-# done
+key_pull_up $KEY_1
+led_pull_off $LED_1
 
 while [ 1 ]
 do
 	key_led_action $KEY_1 $LED_1 
-	#key_led_action $KEY_2 $LED_2 
-	#sleep 0.1
 done
